ae77dfa704cc75e7143b8c7610aeb346b036e7d0
Harry Chan <taiharry108@gmail.com>
2020-05-06 14:32:35 -0700

    change effect for showlist image

--- src/app/show-list/show-list.component.ts
+++ src/app/show-list/show-list.component.ts
@@ -1,10 +1,19 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Show } from '../video-player/show.model';
 import { Observable } from 'rxjs';
 import { ShowService } from '../show/show.service';
 import { Store } from '@ngrx/store';
 import * as fromRoot from '../app.reducer';
 import { Router } from '@angular/router';
+import {
+  style,
+  state,
+  trigger,
+  transition,
+  animate,
+  query,
+  group,
+} from '@angular/animations';
 
 const fakeShows = [
   {
@@ -61,6 +70,28 @@ const fakeShows = [
   selector: 'app-show-list',
   templateUrl: './show-list.component.html',
   styleUrls: ['./show-list.component.scss'],
+  animations: [
+    trigger('bgBlur', [
+      state('no-blur', style({ filter: 'blur(0)' })),
+      state('blur', style({ filter: 'blur(2px)' })),
+      transition('* => *', animate('0.5s')),
+    ]),
+    trigger('bgFilter', [
+      state('no-filter', style({ opacity: 0 })),
+      state('filter', style({ opacity: 0.3 })),
+      transition('* => *', animate('0.5s')),
+    ]),
+    trigger('out2In', [
+      transition('out => in', [
+        style({ transform: 'translateY(-50%)' }),
+        animate('0.5s', style({ transform: 'translateY(-100%)' })),
+      ]),
+      transition('in => out', [
+        style({ transform: 'translateY(-100%)' }),
+        animate('0.5s', style({ transform: 'translateY(-50%)' })),
+      ]),
+    ]),
+  ],
 })
 export class ShowListComponent implements OnInit {
   // @Input() shows: Show[];
@@ -75,7 +106,7 @@ export class ShowListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.showService.fetchLatestShow();    
+    this.showService.fetchLatestShow();
     this.shows$ = this.store.select(fromRoot.getLatestShows);
   }
 
@@ -84,11 +115,25 @@ export class ShowListComponent implements OnInit {
   }
 
   getImgStyle(i: number) {
-    if (i == this.displayIdx) return { filter: "blur(3px)" };
+    if (i == this.displayIdx) return { filter: 'blur(3px)' };
     else return {};
   }
 
   goto(showId: string): void {
     this.router.navigate([`show/${showId}`]);
+  }
+
+  animationDone() {
+    console.log('in done');
+  }
+
+  getBgStyle(thumImgUrl: string) {
+    let style = {
+      background: `url(${thumImgUrl})`,
+      'background-repeat': 'no-repeat',
+      'background-position': '50%',
+      'background-size': '100%',
+    };
+    return style;
   }
 }
